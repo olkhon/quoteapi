cc874d8cf8dcab94c3f83a2ea5240c0d378a8791
olkhon <alexander.stapel@gmail.com>
2021-01-11 13:31:10 +0100

    retrieve needed information from api and set statevariable with respective value

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import axios from 'axios';
 
 function App() {
+
+let [text, setText] = useState('');
+
+useEffect( ()=> {
+  axios
+  .get("https://ron-swanson-quotes.herokuapp.com/v2/quotes")
+  .then( (res) => {
+      let accessObjectValue = Object.values(res);
+      let accessArrayValue = accessObjectValue[0];
+      setText(accessArrayValue)
+  })
+  .catch( (error) => {
+    console.log(error);
+  })
+}, []);
+
+
+
   return (
     <div className="App">
       <h1>
@@ -11,7 +31,7 @@ function App() {
         src="https://media.giphy.com/media/tSVnUxoWoHC/giphy.gif"
         alt="ron"
       />
-      <p>QUOTE</p>
+      <p>Quote: {text}</p>
     </div>
   );
 }
