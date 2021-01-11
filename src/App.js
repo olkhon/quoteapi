01f8f4f2ee7dd85872877c80ca4db5ae04fe0aad
olkhon <alexander.stapel@gmail.com>
2021-01-11 12:33:51 +0100

    insert app content from task

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        <a href="/instructions.html"> instructions </a>
+      </h1>
+      <img
+        src="https://media.giphy.com/media/tSVnUxoWoHC/giphy.gif"
+        alt="ron"
+      />
+      <p>QUOTE</p>
+    </div>
+  );
+}
+
+export default App;
